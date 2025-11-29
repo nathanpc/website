055fd70e787040c008b641edb6ec7f3e952ef5f3
Nathan Campos <nathan@innoveworkshop.com>
2025-11-29 12:43:24 +0000

    Converting images with JPG (capitalized) extension using img-compat-dir.sh

--- bin/img-compat-dir.sh
+++ bin/img-compat-dir.sh
@@ -12,5 +12,6 @@ fi
 imgdir="$1"
 bindir="$(dirname "$0")"
 
-find "$imgdir" -type f \( -name '*.png' -o -name '*.jpg' -o -name '*.bmp' \
-	-o -name '*.jpeg' -o -name '*.webp' \) -exec $bindir/img-compat.sh {} \;
+find "$imgdir" -type f \( -name '*.png' -o -name '*.jpg'  -o -name '*.bmp' \
+	-o -name '*.jpeg' -o -name '*.JPG' -o -name '*.webp' \) \
+	-exec $bindir/img-compat.sh {} \;
