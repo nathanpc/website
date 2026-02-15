fd36ef6beac489217cf44f47954f502d01a773ea
Nathan Campos <nathan@innoveworkshop.com>
2026-02-15 10:25:56 +0000

    Added 3 new log entries

---
+++ gopher/phlog/2026-02-01_winsock2-rcvtimeo/index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+	<?php include __DIR__ . '/../../../templates/head.php'; ?>
+
+	<!-- Page information. -->
+	<title>It has been 0 days since I was bitten by a WinSock2 bug</title>
+</head>
+<body>
+	<?php include_template('header'); ?>
+
+	<div id="blog-post" class="section">
+		<h2>It has been 0 days since I was bitten by a WinSock2 bug</h2>
+		<div id="published-date">2026-02-01 - <a href="gopher://nathancampos.me/0/phlog/2026-02-01_winsock2-rcvtimeo/post.txt">Also available on Gopher</a></div>
+	</div>
+
+<pre id="plain-text">This will be a short one, but I just want to log this and create yet another
+link that can be used to objectively point out that WinSock2 was a mistake.
+
+I love UNIX and POSIX, but there's something about Windows that constantly
+attracts me to it. I guess it's a sense of nostalgia, since it was the operating
+system that I learned how to compute on, or maybe it's simply a matter of
+enjoying painful experiences, I'm not really sure.
+
+Either way, every project that I build, I ensure that it can be compiled/run on
+Windows natively, meaning no Cygwin [<a href="https://cygwin.com/">1</a>], and specially I want to compile it with
+Microsoft's native tools, meaning Visual C++, since that's the correct way of
+doing it, and feels proper, but this comes with a major gotcha: Standards
+compatibility.
+
+I don't know if Microsoft wanted to break compatibility with standards on
+purpose or not, although given their behaviour in the 90's, it most likely was
+on purpose, but their implementation of POSIX and C/C++ standards was a
+nightmare. Some of the issues I've encountered when porting applications over
+have been logged on my wiki [<a href="https://wiki.nathancampos.me/doku.php?id=devnotes:msvc-porting">2</a>], but this one bit me yet again this week...
+
+I'm currently in the process of rewriting, for the third time, my AirDrop-like
+program called GroundLift [<a href="https://github.com/nathanpc/groundlift">3</a>], and in doing so I wanted to port it over to
+Windows and get it to compile under Visual Studio.
+
+Porting networking applications over to Windows is a nightmare that no one
+should have to endure, and Microsoft has tried to document some of the issues
+surrounding this process [<a href="https://learn.microsoft.com/en-us/windows/win32/winsock/porting-socket-applications-to-winsock">4</a>], but at least once a year, I get bitten by the same
+bug: Passing a struct timeval as the value to the SO_RCVTIMEO socket option.
+
+In UNIX sockets you have this really nice option for sockets called SO_RCVTIMEO.
+It allows you to periodically timeout a recv() call, so that you are able to
+shutdown a server cleanly for example. It's a really handy option to use, and
+thus is included in almost any piece of networking code that listens for
+connections.
+
+Under a sane build environment [<a href="https://www.man7.org/linux/man-pages/man7/socket.7.html">5</a>], this option takes a struct timeval [<a href="https://www.man7.org/linux/man-pages/man3/timeval.3type.html">6</a>] that
+represents the time in seconds until socket operations should timeout, but
+Microsoft, in all their wisdom, decided that in their implementation of the
+sockets API, the SO_RCVTIMEO should take a DWORD with the number of milliseconds
+until a timeout occurs [<a href="https://learn.microsoft.com/en-us/windows/win32/api/winsock/nf-winsock-setsockopt">7</a>], and since the setsockopt() function takes a pointer
+to a variable for its parameters, it means that if you use a struct timeval as a
+sane person, and pass it over to setsockopt(), it will compile without warnings
+and the system will simply use the contents of the structure as the timeout
+value, which depending on your code, can lead to very nasty bugs where only some
+connections timeout, ensuring that you pull your hair out while trying to debug
+it.
+
+If you wish for your code to run on both platforms you will have to do something
+like this:
+
+<code>struct timeval tv;
+#ifdef _WIN32
+DWORD dwTimeout;
+#endif /* _WIN32 */
+
+/* Set a reception timeout so that we don't block indefinitely. */
+tv.tv_sec = 2;
+tv.tv_usec = 0;
+#ifdef _WIN32
+dwTimeout = tv.tv_sec * 1000;
+if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&amp;dwTimeout,
+        sizeof(dwTimeout)) == SOCKET_ERROR) {
+    closesocket(sockfd);
+    return -1;
+}
+#else
+if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &amp;tv, sizeof(tv)) == -1) {
+    close(sockfd);
+    return -1;
+}
+#endif /* _WIN32 */</code>
+
+I guess this is it, I just wanted to rant about Microsoft's awful sockets
+implementation. Thanks for wasting your time reading this, I do appreciate it.
+
+
+[1]: <a href="https://cygwin.com/">https://cygwin.com/</a>
+[2]: <a href="https://wiki.nathancampos.me/doku.php?id=devnotes:msvc-porting">https://wiki.nathancampos.me/doku.php?id=devnotes:msvc-porting</a>
+[3]: <a href="https://github.com/nathanpc/groundlift">https://github.com/nathanpc/groundlift</a>
+[4]: <a href="https://learn.microsoft.com/en-us/windows/win32/winsock/porting-socket-applications-to-winsock">https://learn.microsoft.com/en-us/windows/win32/winsock/porting-socket-applications-to-winsock</a>
+[5]: <a href="https://www.man7.org/linux/man-pages/man7/socket.7.html">https://www.man7.org/linux/man-pages/man7/socket.7.html</a>
+[6]: <a href="https://www.man7.org/linux/man-pages/man3/timeval.3type.html">https://www.man7.org/linux/man-pages/man3/timeval.3type.html</a>
+[7]: <a href="https://learn.microsoft.com/en-us/windows/win32/api/winsock/nf-winsock-setsockopt">https://learn.microsoft.com/en-us/windows/win32/api/winsock/nf-winsock-setsockopt</a>
+</pre>
+
+	<?php include_template('footer'); ?>
+</body>
+</html>
