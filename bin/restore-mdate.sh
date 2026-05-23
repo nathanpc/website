14335cf79e723f45fc396acd6407e73b29be30cd
Nathan Campos <nathan@innoveworkshop.com>
2026-05-23 10:34:25 +0100

    Added a script to restore the modified date of files in Git

---
+++ bin/restore-mdate.sh
@@ -0,0 +1,36 @@
+#!/bin/sh -e
+
+## restore-mdate.sh
+## Restores the last modified date for files when using Git.
+##
+## Source: https://gist.github.com/HackingGate/9e8169c7645b074b2f40c959ca20d738
+
+OS=${OS:-`uname`}
+
+if [ "$OS" = 'Darwin' ]; then
+  get_touch_time() {
+    date -r ${unixtime} +'%Y%m%d%H%M.%S'
+  }
+else
+  # default Linux
+  get_touch_time() {
+    date -d @${unixtime} +'%Y%m%d%H%M.%S'
+  }
+fi
+
+# all git files
+git ls-tree -r --name-only HEAD	> .git_ls-tree_r_name-only_HEAD
+
+# modified git files
+git diff --name-only > .git_diff_name-only
+
+# minus https://stackoverflow.com/questions/1370996/minus-operation-on-two-files-using-linux-commands                                                                      
+# only restore files not modified
+comm -2 -3 .git_ls-tree_r_name-only_HEAD .git_diff_name-only | while read filename; do
+  unixtime=$(git log -1 --format="%at" -- "${filename}")
+  touchtime=$(get_touch_time)
+  echo ${touchtime} "${filename}"
+  touch -t ${touchtime} "${filename}"
+done
+
+rm .git_ls-tree_r_name-only_HEAD .git_diff_name-only
