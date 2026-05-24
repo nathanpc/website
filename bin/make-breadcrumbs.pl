770293ea55452fa1014e7c4be126431de9b7d15d
Nathan Campos <nathan@innoveworkshop.com>
2026-05-24 12:29:42 +0100

    Started building a script to build breadcrumbs from a path
    We need the fancy breadcrumbs for the header of our website, so this
    script will take care of building this functionality.

---
+++ bin/make-breadcrumbs.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/env perl
+
+## make-breadcrumbs.pl
+## Writes the HTML breadcrumbs section for a given a file.
+##
+## Author: Nathan Campos <hi@nathancampos.me>
+
+use strict;
+use warnings;
+use autodie;
+
+use File::Basename;
+use Term::ANSIColor;
+use Cwd qw(abs_path);
+
+# Get the base htdocs folder path
+our $htdocs = abs_path(dirname(__FILE__) . '/../htdocs/');
+
+# Makes the breadcrumbs HTML from a normalized string (relative to htdocs).
+sub make_crumbs {
+	my ($relpath) = @_;
+	my @crumbs = split /\//, $relpath;
+	my $sep = '<span class="sep">/</span>';
+
+	# Go through the path splits and build the crumbs.
+	my $html = '';
+	while (scalar(@crumbs) > 0) {
+		my $href = join '/', @crumbs;
+		my $label = pop @crumbs;
+
+		# Ignore some instances.
+		next if (length $label == 0);
+		next if ($label =~ /^index\.(php|html?)/);
+		
+		$html = " $sep <a class=\"label\" href=\"$href\">$label</a>" . $html;
+	}
+
+	# Print out the built HTML.
+	$html = substr $html, 1;
+	print "<div id=\"breadcrumbs\">\n\t$html\n</div>\n";
+}
+
+# Ensure we have a filename to get the relative path from.
+if (scalar(@ARGV) < 1) {
+	print "usage: $0 fname\n";
+	exit 1;
+}
+my $fname = abs_path($ARGV[0]);
+
+# Check if the path is not a subfolder of htdocs.
+if (rindex($fname, $htdocs, 0) < 0) {
+	print STDERR colored("ERROR: Path $ARGV[0] is not a subfolder of htdocs.",
+		'red'), "\n";
+	exit 1;
+}
+
+# Build up the breadcrumbs HTML.
+make_crumbs(substr $fname, length($htdocs));
