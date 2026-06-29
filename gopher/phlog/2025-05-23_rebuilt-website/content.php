453104b600e64f2c33af70e12d91c2ead76c65c9
Nathan Campos <nathan@innoveworkshop.com>
2026-06-29 16:44:44 +0100

    Updated some blog posts to use regular HTML

--- gopher/phlog/2025-05-23_rebuilt-website/content.php
+++ gopher/phlog/2025-05-23_rebuilt-website/content.php
@@ -1,34 +1,31 @@
 <!-- Rebuilt the website (again) -->
 <!-- Also available on Gopher: gopher://nathancampos.me/0/phlog/2025-05-23_rebuilt-website/post.txt -->
 
-<pre id="plain-text">I guess the only I do with this website is rebuild it... Once again I decided to
+<p>I guess the only I do with this website is rebuild it... Once again I decided to
 redo the entire thing. In less than a year this website has been built from
-scratch, and rebuilt completely twice.
+scratch, and rebuilt completely twice.</p>
 
-The reason for the rebuild this time was out of frustration. Even though I was
-fairly happy with the static website I had built [<a href="http://nathancampos.me/log/2024-11-15_this-as-static-website/">1</a>], I couldn't really edit it
+<p>The reason for the rebuild this time was out of frustration. Even though I was
+fairly happy with <a href="<?= blog_href('2024-11-15', 'this-as-static-website') ?>">the
+static website I had built</a>, I couldn't really edit it
 and test my changes on any of my old computers, largely because of the
 requirement on PHP 8. This made me very frustrated, since one of the joys of
 having such a simple and minimalistic website is to use it and edit it on older
-machines.
+machines.</p>
 
-After much frustration I did decide to pull the trigger on the rewrite and
-basically reverted everything to how things were before the static rewrite [<a href="https://github.com/nathanpc/portfolio/tree/97b1234e2da81719dd5fa9142bd4c7779162def2">2</a>],
+<p>After much frustration I did decide to pull the trigger on the rewrite and
+basically <a href="https://github.com/nathanpc/portfolio/tree/97b1234e2da81719dd5fa9142bd4c7779162def2">reverted
+everything to how things were before the static rewrite</a>,
 although I made a whole bunch of changes to make it more maintainable in the
 long run, especially since the old code for the website was littered with
-special functions for blog posts that I had to look up every time I made a post.
+special functions for blog posts that I had to look up every time I made a post.</p>
 
-All in all the website is now in a much better state, the code for it is
+<p>All in all the website is now in a much better state, the code for it is
 cleaner, it's easier to maintain, many of the processes used to administer it
 adhere to the UNIX philosophy, and I can basically work on it on any machine I
-desire.
+desire.</p>
 
-Maybe one day I will implement an RSS feed generator for the blog and actually
-make use of those compat_&ast; functions [<a href="https://github.com/nathanpc/portfolio/blob/52b256478712c2d6552bb1d93e0095c2aabea71c/src/compat.php">3</a>] that are supposed to make the website
-work on older web browsers.
-
-[1]: <a href="http://nathancampos.me/log/2024-11-15_this-as-static-website/">http://nathancampos.me/log/2024-11-15_this-as-static-website/</a>
-[2]: <a href="https://github.com/nathanpc/portfolio/tree/97b1234e2da81719dd5fa9142bd4c7779162def2">https://github.com/nathanpc/portfolio/tree/97b1234e2da81719dd5fa9142bd4c7779162def2</a>
-[3]: <a href="https://github.com/nathanpc/portfolio/blob/52b256478712c2d6552bb1d93e0095c2aabea71c/src/compat.php">https://github.com/nathanpc/portfolio/blob/52b256478712c2d6552bb1d93e0095c2aabea71c/src/compat.php</a>
-</pre>
-
+<p>Maybe one day I will implement an RSS feed generator for the blog and actually
+make use of <a href="https://github.com/nathanpc/portfolio/blob/52b256478712c2d6552bb1d93e0095c2aabea71c/src/compat.php">those
+<code>compat_&ast;</code> functions</a> that are supposed to make the website
+work on older web browsers.</p>
