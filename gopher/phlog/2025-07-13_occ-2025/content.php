c7dbf72af351fabf783332bd9b95f794b62dd63d
Nathan Campos <nathan@innoveworkshop.com>
2026-07-04 15:03:21 +0100

    blog: Old Computer Challenge 2025

---
+++ gopher/phlog/2025-07-13_occ-2025/content.php
@@ -0,0 +1,20 @@
+<!-- Old Computer Challenge 2025 -->
+
+<p>Although this post is dated 2025-07-13, the date when the
+<a href="http://occ.sdf.org/#2025">2025 edition of the Old Computer
+Challenge</a> took place, this post was actually written in 2026-07-04, the day
+before the 2026 edition of the challenge is set to begin.</p>
+
+<p>The reason for this date discrepancy is that, during OCC 2025 I decided to
+log all my retro adventures on my <a href="gopher://nathancampos.me/1/">gopherhole</a>,
+so there was no other way to get to this content via HTTP. I was certain I had
+at least published a post about this here, but apparently I forgot, so before
+the next edition of the challenge takes place, I decided to make this post so
+other participants that are not on Gopher can at least know where to get to my
+adventures from the previous year.</p>
+
+<p>Although I'm not going to republish any of the content from that edition
+here, you can either <a href="gopher://nathancampos.me/1/occ/2025">use this link
+if you have a Gopher client</a>, or if you don't and are unwilling to install
+one, you can <a href="https://gopher.floodgap.com/gopher/gw?a=gopher%3A%2F%2Fnathancampos.me%2F1%2Focc%2F2025">use
+this link to access the content via a proxy</a>.</p>
